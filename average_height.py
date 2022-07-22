92c2dd6c9f89eb98ad2ae791371219b97103ec19
Kamanzi Rama-Munroe <kmunroe@mailfence.com>
2022-07-21 23:54:47 -0400

    include sum loop and formula to get average

--- average_height.py
+++ average_height.py
@@ -1,4 +1,12 @@
-fruits = ["apple", "peach", "pair"]
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
 
-for fruit in fruits:
-    print(fruit)
+height_sum = 0
+
+for avg in student_heights:
+  height_sum += avg
+
+height_avg = round(height_sum / len(student_heights))
+
+print(height_avg)
